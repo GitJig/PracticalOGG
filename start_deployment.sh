a6ab279c165f5ac38fc2944047ec9418658e1131
Jigar Doshi <jigardoshi79@gmail.com>
2023-03-30 04:27:24 +0000

    added start script

---
+++ start_deployment.sh
@@ -0,0 +1,34 @@
+export compname=JDBlog
+export Compocid=`oci iam compartment list --compartment-id-in-subtree true --all |jq -r ".data[] | select(.name == \"${compname}\") | .id"`
+
+# List Deployments and Status
+oci goldengate deployment list --compartment-id $Compocid --all --query 'data.items[*].{DeploymentName:"display-name",Status:"lifecycle-state"}' --output table
+
+echo "Enter Deployment Name to Start"
+read deploymentname
+echo "Starting OCI GG Deployment $deploymentname"
+export Deploymentocid=`oci goldengate deployment list --compartment-id $Compocid --all --query "data.items[?contains(\"display-name\",'$deploymentname')].id"|jq -r '.[]'`
+if [ -z "$Deploymentocid" ]
+then
+	echo "Deployment $deploymentname not found in $compname"
+	exit
+else 
+echo $Deploymentocid
+fi
+
+export WorkReqOCID=`oci goldengate deployment start --deployment-id $Deploymentocid |jq -r '."opc-work-request-id"'`
+
+while true
+do 
+export status=`oci goldengate work-request get --work-request-id $WorkReqOCID|jq -r ".data.status"`
+echo "OCI GoldenGate startup request for $deploymentname is -- $status"
+if [ $status=="SUCCEEDED" ]
+then	
+	exit
+else
+	sleep 20
+fi
+echo "Sleeping for 20 seconds"
+echo "Ctrl-C to exit"
+sleep 20
+done 
