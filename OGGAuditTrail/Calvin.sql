7a1e90f4cd2280118c8b36eb2c6f5f55e1b76d4a
Jigar Doshi <jigardoshi79@gmail.com>
2024-11-27 11:55:52 +0800

    Create Calvin.sql

---
+++ OGGAuditTrail/Calvin.sql
@@ -0,0 +1,9 @@
+Conn calvin/password@TNS
+
+Insert into Appowner.DEPARTMENT values (1,'COLLECTIONS','FIN','Singapore','SGCF');
+Insert into Appowner.DEPARTMENT values (2,'PAYMENT','FIN','Singapore','SGPF');
+Insert into Appowner.DEPARTMENT values (3,'ADMIN','OPS','Singapore','SGAO');
+
+Insert into Appowner.EMP values ( 1,'MOE','BULLY','ADMIN',1,'01-JAN-1985','AMERICA','AMERICA','ACTIVE'); 
+Insert into Appowner.EMP values ( 2,'Wormwood','BULLY','ADMIN',1,'01-JAN-1925','AMERICA','AMERICA','ACTIVE'); 
+Insert into Appowner.EMP values ( 3,'Rosalyn','BULLY','ADMIN',1,'01-JAN-1975','AMERICA','AMERICA','ACTIVE'); 
